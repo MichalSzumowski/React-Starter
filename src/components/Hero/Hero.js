cd99639a80a2910ef4e31d49f8ab04f0d1e86cce
Michał Szumowski <mic.szumowski@gmail.com>
2021-07-13 21:39:04 +0200

    Add Cards component - exercise 15.5

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,11 +2,15 @@ import React from 'react';
 import styles from './Hero.scss';
 import PropTypes from 'prop-types';
 
+import ReactHtmlParser from 'react-html-parser';
+import { listData } from '../../data/dataStore';
+
+
 
 const Hero = props => (
     <header className={styles.component}>
-      <h2 className={styles.title}>{props.titleText}</h2>
-      <img src={props.srcText} alt="alt" className={styles.image} />
+      <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
+      <img src={listData.image} alt="alt" className={styles.image} />
     </header>
   );
   
